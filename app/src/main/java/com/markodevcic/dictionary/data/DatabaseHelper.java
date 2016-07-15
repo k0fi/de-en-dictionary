031573ce1f29bf8b00e11e5e7edf784466159699
Marko Devcic <madevcic@gmail.com>
2016-07-15 22:29:11 +0200

    check if unsubcscribed on moveToFirst

--- app/src/main/java/com/markodevcic/dictionary/data/DatabaseHelper.java
+++ app/src/main/java/com/markodevcic/dictionary/data/DatabaseHelper.java
@@ -75,9 +75,9 @@ public final class DatabaseHelper extends SQLiteOpenHelper {
 			@Override
 			public void call(Subscriber<? super Pair<String, String>> subscriber) {
 				SQLiteDatabase database = getReadableDatabase();
-				String sql = "SELECT * FROM " + TABLE_FTS + " WHERE " + TABLE_FTS + " MATCH ? ORDER BY MATCHINFO(" + TABLE_FTS +")  DESC LIMIT 40 OFFSET 0";
+				String sql = "SELECT * FROM " + TABLE_FTS + " WHERE " + TABLE_FTS + " MATCH ? ORDER BY MATCHINFO(" + TABLE_FTS +", 'x')  DESC LIMIT 40 OFFSET 0";
 				Cursor cursor = database.rawQuery(sql, new String[]{query});
-				if (cursor.moveToFirst()) {
+				if (cursor.moveToFirst() && !subscriber.isUnsubscribed()) {
 					do {
 						String deText = cursor.getString(cursor.getColumnIndex(COL_DE_TEXT));
 						String enText = cursor.getString(cursor.getColumnIndex(COL_EN_TEXT));
