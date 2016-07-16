ea43460b423626e32ef9f500706b5fafa258c652
Marko Devcic <madevcic@gmail.com>
2016-07-16 14:50:45 +0200

    add startup activity layout

--- app/src/main/java/com/markodevcic/dictionary/activity/StartupActivity.java
+++ app/src/main/java/com/markodevcic/dictionary/activity/StartupActivity.java
@@ -1,11 +1,17 @@
 package com.markodevcic.dictionary.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.markodevcic.dictionary.R;
 import com.markodevcic.dictionary.data.DatabaseHelper;
 import com.markodevcic.dictionary.injection.AppComponent;
 
 import javax.inject.Inject;
+
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class StartupActivity extends BaseActivity {
 
@@ -15,6 +21,31 @@ public class StartupActivity extends BaseActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		if (databaseHelper.getRowCount() > 0) {
+			Intent intent = new Intent(this, MainActivity.class);
+			startActivity(intent);
+		} else {
+			setContentView(R.layout.activity_startup);
+			databaseHelper.startLoadDictionary()
+					.subscribeOn(Schedulers.io())
+					.observeOn(AndroidSchedulers.mainThread())
+					.subscribe(new Observer<Void>() {
+						@Override
+						public void onCompleted() {
+
+						}
+
+						@Override
+						public void onError(Throwable e) {
+
+						}
+
+						@Override
+						public void onNext(Void aVoid) {
+
+						}
+					});
+		}
 	}
 
 	@Override
