3c0ef861b0a829f4a3cbd3355d6f1b43f32d4e95
Marko Devcic <madevcic@gmail.com>
2016-07-21 17:12:38 +0200

    add strict mode

--- app/src/main/java/com/markodevcic/dictionary/DictionaryApplication.java
+++ app/src/main/java/com/markodevcic/dictionary/DictionaryApplication.java
@@ -1,10 +1,21 @@
 package com.markodevcic.dictionary;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 public final class DictionaryApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		if (BuildConfig.DEBUG) {
+			StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+					.detectAll()
+					.penaltyLog()
+					.build());
+			StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+					.detectAll()
+					.penaltyLog()
+					.build());
+		}
 	}
 }
